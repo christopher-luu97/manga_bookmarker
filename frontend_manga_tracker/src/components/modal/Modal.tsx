b4269c32986c471eef457fa0cc5aa2502fdddbf9
christopher-luu97 <christopher.luu1337@gmail.com>
2023-12-22 12:22:40 +1100

    Add dynamic resizing on table

--- frontend_manga_tracker/src/components/modal/Modal.tsx
+++ frontend_manga_tracker/src/components/modal/Modal.tsx
@@ -41,7 +41,10 @@ export const Modal: React.FC<{
 
   return (
     <div className="fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full z-50 backdrop-blur-sm">
-      <div className="relative top-20 mx-auto p-5 border w-2/3 shadow-lg rounded-md bg-white z-50">
+      <div
+        className="relative top-20 mx-auto p-5 border shadow-lg rounded-md bg-white z-50"
+        style={{ width: "80%" }}
+      >
         <button
           onClick={onClose}
           className="absolute top-2 right-3 text-xl text-gray-600 hover:text-gray-800"
@@ -66,52 +69,54 @@ export const Modal: React.FC<{
             Add
           </button>
         </div>
-        <table className="min-w-full text-left border-collapse border border-gray-300">
-          <thead>
-            <tr className="bg-gray-100">
-              <th className="border px-4 py-2">Title</th>
-              <th className="border px-4 py-2">Link</th>
-              <th className="border px-4 py-2">Status</th>
-              <th className="border px-4 py-2">Delete</th>
-            </tr>
-          </thead>
-          <tbody>
-            {draftData.map((manga) => (
-              <tr key={manga.id}>
-                <td className="border px-4 py-2 max-w-xs overflow-auto whitespace-nowrap">
-                  {manga.title}
-                </td>
-                <td className="border px-4 py-2 max-w-xs overflow-auto whitespace-nowrap">
-                  <a
-                    href={manga.link}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="text-blue-500 hover:underline"
-                    style={{ maxWidth: "50ch" }}
-                  >
-                    {manga.link}
-                  </a>
-                </td>
-                <td className="border px-4 py-2 text-center">
-                  <span
-                    className={`inline-block h-4 w-4 rounded-full mr-2 ${getStatusColor(
-                      manga.status
-                    )}`}
-                    title={manga.status}
-                  ></span>
-                </td>
-                <td className="border px-4 py-2">
-                  <button
-                    onClick={() => handleDelete(manga.id)}
-                    className="text-red-500 hover:text-red-700"
-                  >
-                    Delete
-                  </button>
-                </td>
+        <div className="overflow-auto">
+          <table className="min-w-full text-left border-collapse border border-gray-300">
+            <thead>
+              <tr className="bg-gray-100">
+                <th className="border px-4 py-2">Title</th>
+                <th className="border px-4 py-2">Link</th>
+                <th className="border px-4 py-2">Status</th>
+                <th className="border px-4 py-2">Delete</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {draftData.map((manga) => (
+                <tr key={manga.id}>
+                  <td className="border px-4 py-2 max-w-xs overflow-auto whitespace-nowrap">
+                    {manga.title}
+                  </td>
+                  <td className="border px-4 py-2 max-w-xs overflow-auto whitespace-nowrap">
+                    <a
+                      href={manga.link}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="text-blue-500 hover:underline"
+                      style={{ maxWidth: "50ch" }}
+                    >
+                      {manga.link}
+                    </a>
+                  </td>
+                  <td className="border px-4 py-2 text-center">
+                    <span
+                      className={`inline-block h-4 w-4 rounded-full mr-2 ${getStatusColor(
+                        manga.status
+                      )}`}
+                      title={manga.status}
+                    ></span>
+                  </td>
+                  <td className="border px-4 py-2">
+                    <button
+                      onClick={() => handleDelete(manga.id)}
+                      className="text-red-500 hover:text-red-700"
+                    >
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
         <div className="flex justify-between mt-4">
           <button
             onClick={handleConfirm}
