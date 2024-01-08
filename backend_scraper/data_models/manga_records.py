64307ab08cdc54869fc0cc902a965de72e1fa9a4
christopher-luu97 <christopher.luu1337@gmail.com>
2024-01-08 18:45:08 +1100

    Add documentation to manga_records to indicate what each record means. manga_data.json edited to reflect new status update for deletion. Edit modal input to edit data to be sent to backend for deleting records

--- backend_scraper/data_models/manga_records.py
+++ backend_scraper/data_models/manga_records.py
@@ -6,7 +6,10 @@ class MangaRecord(BaseModel):
     id: str
     lastChecked: str
     link: str
-    status: str
+    # If status is good then is fine, if status == "Delete" then the user can delete these records from the database
+    # We will use the status flag to indicate what database operations to use
+    # This flag is changed based on frontend modal operations
+    status: str 
     title: str
 
 class MangaList(BaseModel):
