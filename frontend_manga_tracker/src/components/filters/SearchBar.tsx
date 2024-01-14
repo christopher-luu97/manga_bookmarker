03fb59b5018741f6cad65fd43e72f857683d5135
christopher-luu97 <christopher.luu1337@gmail.com>
2024-01-14 22:31:48 +1100

    Add dynamic string search to filter result grid

--- frontend_manga_tracker/src/components/filters/SearchBar.tsx
+++ frontend_manga_tracker/src/components/filters/SearchBar.tsx
@@ -1,10 +1,12 @@
-// src/components/SearchBar.tsx
-import React from "react";
-
-export const SearchBar: React.FC = () => {
+export const SearchBar: React.FC<{
+  searchTerm: string;
+  onSearchChange: (searchTerm: string) => void;
+}> = ({ searchTerm, onSearchChange }) => {
   return (
     <input
       type="text"
+      value={searchTerm}
+      onChange={(e) => onSearchChange(e.target.value)}
       placeholder="Search..."
       className="p-2 border border-gray-300 rounded w-full md:w-auto"
     />
