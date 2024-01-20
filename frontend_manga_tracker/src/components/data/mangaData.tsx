a53c9e72ca4dae94b280ac7f659259bb0ee39904
christopher-luu97 <christopher.luu1337@gmail.com>
2024-01-20 21:30:33 +1100

    Add interface for manga data. Add smart filtering with date and name on result grid

--- frontend_manga_tracker/src/components/data/mangaData.tsx
+++ frontend_manga_tracker/src/components/data/mangaData.tsx
@@ -1,118 +1,127 @@
-// src/data/mangaData.ts
-// TODO: Created 10 items as test data
-export const mangaData = [
-  {
-    id: 1,
-    title: "Overgeared 1",
-    chapter: "Chapter 10",
-    lastUpdated: "2023-03-01",
-    imageUrl:
-      "https://w44.overgeared.club/wp-content/uploads/2021/04/Overgeared-Manga-333x450.jpg",
-    link: "https://google.com",
-    status: "Good",
-  },
-  {
-    id: 2,
-    title: "One Piece 1",
-    chapter: "Chapter 10",
-    lastUpdated: "2023-03-01",
-    imageUrl: "https://m.media-amazon.com/images/I/91CN0HstfNL._SY466_.jpg",
-    link: "https://google.com",
-    status: "Down",
-  },
-  {
-    id: 3,
-    title: "Overgeared 2",
-    chapter: "Chapter 10",
-    lastUpdated: "2023-03-01",
-    imageUrl:
-      "https://w44.overgeared.club/wp-content/uploads/2021/04/Overgeared-Manga-333x450.jpg",
-    link: "https://google.com",
-    status: "Good",
-  },
-  {
-    id: 4,
-    title: "One Piece 2",
-    chapter: "Chapter 10",
-    lastUpdated: "2023-03-01",
-    imageUrl: "https://m.media-amazon.com/images/I/91CN0HstfNL._SY466_.jpg",
-    link: "https://google.com",
-    status: "Down",
-  },
-  {
-    id: 5,
-    title: "Overgeared 3",
-    chapter: "Chapter 10",
-    lastUpdated: "2023-03-01",
-    imageUrl:
-      "https://w44.overgeared.club/wp-content/uploads/2021/04/Overgeared-Manga-333x450.jpg",
-    link: "https://google.com",
-    status: "Good",
-  },
-  {
-    id: 6,
-    title: "One Piece 3",
-    chapter: "Chapter 10",
-    lastUpdated: "2023-03-01",
-    imageUrl: "https://m.media-amazon.com/images/I/91CN0HstfNL._SY466_.jpg",
-    link: "https://google.com",
-    status: "Down",
-  },
-  {
-    id: 7,
-    title: "Overgeared 4",
-    chapter: "Chapter 10",
-    lastUpdated: "2023-03-01",
-    imageUrl:
-      "https://w44.overgeared.club/wp-content/uploads/2021/04/Overgeared-Manga-333x450.jpg",
-    link: "https://google.com",
-    status: "Good",
-  },
-  {
-    id: 8,
-    title: "One Piece 4",
-    chapter: "Chapter 10",
-    lastUpdated: "2023-03-01",
-    imageUrl: "https://m.media-amazon.com/images/I/91CN0HstfNL._SY466_.jpg",
-    link: "https://google.com",
-    status: "Down",
-  },
-  {
-    id: 9,
-    title: "Overgeared 5",
-    chapter: "Chapter 10",
-    lastUpdated: "2023-03-01",
-    imageUrl:
-      "https://w44.overgeared.club/wp-content/uploads/2021/04/Overgeared-Manga-333x450.jpg",
-    link: "https://google.com",
-    status: "Good",
-  },
-  {
-    id: 10,
-    title: "One Piece 5",
-    chapter: "Chapter 10",
-    lastUpdated: "2023-03-01",
-    imageUrl: "https://m.media-amazon.com/images/I/91CN0HstfNL._SY466_.jpg",
-    link: "https://google.com",
-    status: "Down",
-  },
-  {
-    id: 11,
-    title: "Overgeared 6",
-    chapter: "Chapter 10",
-    lastUpdated: "2023-03-01",
-    imageUrl:
-      "https://w44.overgeared.club/wp-content/uploads/2021/04/Overgeared-Manga-333x450.jpg",
-    link: "https://google.com",
-    status: "Good",
-  },
-  {
-    id: 12,
-    title: "One Piece 6",
-    chapter: "Chapter 10",
-    lastUpdated: "2023-03-01",
-    imageUrl: "https://m.media-amazon.com/images/I/91CN0HstfNL._SY466_.jpg",
-    link: "https://google.com",
-    status: "Down",
-  },
-];
+// // src/data/mangaData.ts
+export interface mangaDataInterface {
+  id: string;
+  title: string;
+  link: string;
+  lastUpdated: string;
+  imageUrl: string;
+  status: string;
+  chapter_number: string;
+}
+// // TODO: Created 10 items as test data
+// export const mangaData = [
+//   {
+//     id: 1,
+//     title: "Overgeared 1",
+//     chapter: "Chapter 10",
+//     lastUpdated: "2023-03-01",
+//     imageUrl:
+//       "https://w44.overgeared.club/wp-content/uploads/2021/04/Overgeared-Manga-333x450.jpg",
+//     link: "https://google.com",
+//     status: "Good",
+//   },
+//   {
+//     id: 2,
+//     title: "One Piece 1",
+//     chapter: "Chapter 10",
+//     lastUpdated: "2023-03-01",
+//     imageUrl: "https://m.media-amazon.com/images/I/91CN0HstfNL._SY466_.jpg",
+//     link: "https://google.com",
+//     status: "Down",
+//   },
+//   {
+//     id: 3,
+//     title: "Overgeared 2",
+//     chapter: "Chapter 10",
+//     lastUpdated: "2023-03-01",
+//     imageUrl:
+//       "https://w44.overgeared.club/wp-content/uploads/2021/04/Overgeared-Manga-333x450.jpg",
+//     link: "https://google.com",
+//     status: "Good",
+//   },
+//   {
+//     id: 4,
+//     title: "One Piece 2",
+//     chapter: "Chapter 10",
+//     lastUpdated: "2023-03-01",
+//     imageUrl: "https://m.media-amazon.com/images/I/91CN0HstfNL._SY466_.jpg",
+//     link: "https://google.com",
+//     status: "Down",
+//   },
+//   {
+//     id: 5,
+//     title: "Overgeared 3",
+//     chapter: "Chapter 10",
+//     lastUpdated: "2023-03-01",
+//     imageUrl:
+//       "https://w44.overgeared.club/wp-content/uploads/2021/04/Overgeared-Manga-333x450.jpg",
+//     link: "https://google.com",
+//     status: "Good",
+//   },
+//   {
+//     id: 6,
+//     title: "One Piece 3",
+//     chapter: "Chapter 10",
+//     lastUpdated: "2023-03-01",
+//     imageUrl: "https://m.media-amazon.com/images/I/91CN0HstfNL._SY466_.jpg",
+//     link: "https://google.com",
+//     status: "Down",
+//   },
+//   {
+//     id: 7,
+//     title: "Overgeared 4",
+//     chapter: "Chapter 10",
+//     lastUpdated: "2023-03-01",
+//     imageUrl:
+//       "https://w44.overgeared.club/wp-content/uploads/2021/04/Overgeared-Manga-333x450.jpg",
+//     link: "https://google.com",
+//     status: "Good",
+//   },
+//   {
+//     id: 8,
+//     title: "One Piece 4",
+//     chapter: "Chapter 10",
+//     lastUpdated: "2023-03-01",
+//     imageUrl: "https://m.media-amazon.com/images/I/91CN0HstfNL._SY466_.jpg",
+//     link: "https://google.com",
+//     status: "Down",
+//   },
+//   {
+//     id: 9,
+//     title: "Overgeared 5",
+//     chapter: "Chapter 10",
+//     lastUpdated: "2023-03-01",
+//     imageUrl:
+//       "https://w44.overgeared.club/wp-content/uploads/2021/04/Overgeared-Manga-333x450.jpg",
+//     link: "https://google.com",
+//     status: "Good",
+//   },
+//   {
+//     id: 10,
+//     title: "One Piece 5",
+//     chapter: "Chapter 10",
+//     lastUpdated: "2023-03-01",
+//     imageUrl: "https://m.media-amazon.com/images/I/91CN0HstfNL._SY466_.jpg",
+//     link: "https://google.com",
+//     status: "Down",
+//   },
+//   {
+//     id: 11,
+//     title: "Overgeared 6",
+//     chapter: "Chapter 10",
+//     lastUpdated: "2023-03-01",
+//     imageUrl:
+//       "https://w44.overgeared.club/wp-content/uploads/2021/04/Overgeared-Manga-333x450.jpg",
+//     link: "https://google.com",
+//     status: "Good",
+//   },
+//   {
+//     id: 12,
+//     title: "One Piece 6",
+//     chapter: "Chapter 10",
+//     lastUpdated: "2023-03-01",
+//     imageUrl: "https://m.media-amazon.com/images/I/91CN0HstfNL._SY466_.jpg",
+//     link: "https://google.com",
+//     status: "Down",
+//   },
+// ];
