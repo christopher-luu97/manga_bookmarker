a9c0e5da34398b3c976eac06e73fbd3cd0661eee
christopher-luu97 <christopher.luu1337@gmail.com>
2024-02-06 07:31:52 +1100

    Add website name to stored proc

--- database/stored_procs/insert_website.sql
+++ database/stored_procs/insert_website.sql
@@ -1,5 +1,6 @@
 CREATE OR REPLACE PROCEDURE insert_website(
     p_website_id UUID,
+    p_website_name VARCHAR,
     p_website_url VARCHAR,
     p_website_status VARCHAR,
     p_date_checked TIMESTAMP
@@ -9,11 +10,13 @@ AS $$
 BEGIN
     INSERT INTO website_table (
         website_id,
+        website_name,
         website_url,
         website_status,
         date_checked
     ) VALUES (
         p_website_id,
+        p_website_name,
         p_website_url,
         p_website_status,
         p_date_checked
